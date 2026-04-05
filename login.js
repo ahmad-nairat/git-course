1cac09185676df506038c237c4c9a5563d21210f
Ahmad Nairat <nairat@ahmadnairat.dev>
2026-04-05 17:26:47 +0300

    update username and password in login

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 const userName = "smth",
   password = "smth";
 
-if (userName === "smth" && password === "smth") {
+if (userName === "admin" && password === "12345") {
   console.log("Welcome back!");
 }

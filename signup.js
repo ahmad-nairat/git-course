6ae069c17610a24a3d7a57c265ec87986e5d2f47
Ahmad Nairat <nairat@ahmadnairat.dev>
2026-04-05 17:23:31 +0300

    smth

--- signup.js
+++ signup.js
@@ -4,3 +4,8 @@ if (userName === '' || password === '' || confirmPassword === '') {
     console.log('Please fill in all fields.');
 }
 
+if (password !== confirmPassword) {
+    console.log('Passwords do not match.');
+}
+
+console.log('Signup successful!');
